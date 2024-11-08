c9abc0322cb225b16fa0728ce60687e128778495
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2024-11-08 19:51:34 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('<button>Click me</button>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
